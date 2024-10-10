f80babebc1695f5a3162df83c6170591cae9d50a
Jou Okuyama <ojoxux@gmail.com>
2024-10-10 17:43:19 +0900

    chore🛠️: VitestのテストにPlaywright用のディレクトリが読み込まれていたので除外するように変更

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
+    include: ["src/**/*.test.jsx"], // テストファイルのパターンを指定
+    exclude: ["tests/**/*.spec.js"], // 除外するファイルを指定
+  },
+});
